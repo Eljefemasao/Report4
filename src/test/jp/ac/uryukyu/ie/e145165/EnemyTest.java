13261516e9f1b1c5468d87de62607c9cb0a6aee9
Masaaki Matsunaga <castle2164m@gmail.com>
2017-01-02 01:29:10 +0900

    programing2 report4 ステップ０：コードの準備

---
+++ src/test/jp/ac/uryukyu/ie/e145165/EnemyTest.java
@@ -0,0 +1,23 @@
+package jp.ac.uryukyu.ie.e145165;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by masaaki on 2017/01/02.
+ */
+public class EnemyTest {
+    @org.junit.Test
+    public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
+    }
+
+    }
+
